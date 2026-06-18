62aa05638a1e4d973ad8c404cdfb923e3188a659
belamx <belammuia0@gmail.com>
2026-06-18 17:54:52 +0300

    add login path to the urls

--- api/accounts/urls.py
+++ api/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 app_name = 'football'
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path('register/club/', views.ClubRegistrationView.as_view(), name='register-club'),
     path('teams/register/', views.TeamRegistrationView.as_view(), name='register-team'),
     path('users/<int:pk>/verify/', views.VerifyUserView.as_view(), name='verify-user'),
+    path('login/', TokenObtainPairView.as_view(), name='login-user'),
 ]
